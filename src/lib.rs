8b708a2ae4b79e61cae470dc03ef521cd546aef3
freeliujian <670177821@qq.com>
2025-05-06 19:13:44 +0800

    feat: 添加一些配置

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,54 @@
-use std::rc::Rc;
-
+use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::prelude::*;
 use web_sys::Element;
 use gloo::utils::{document};
 
+#[wasm_bindgen]
+#[derive(Clone)]
 pub struct IWordInfo {
+    #[wasm_bindgen(getter_with_clone)] 
     pub text: Vec<String>,
+    #[wasm_bindgen(getter_with_clone)] 
     pub widths: Vec<usize>,
     pub font_size: usize,
     pub ellipsis: bool,
 }
 
+#[wasm_bindgen]
+#[derive(Clone)]
 pub struct LineClampProps {
+    #[wasm_bindgen(getter_with_clone)] 
     pub texts: Vec<String>,
     pub contentWidth: i32,
     pub contentHeight: i32,
 }
-
+#[wasm_bindgen]
 pub struct InputResultForEllipsis {
+    #[wasm_bindgen(getter_with_clone)] 
     pub html: Vec<String>,
     pub font_size: i32,
 }
 
+#[wasm_bindgen]
 pub struct LineClamp {
+    #[wasm_bindgen(getter_with_clone)] 
     pub element: Element,
     pub step: f32,
     pub minFontSize: i32,
     pub maxFontSize: i32,
     pub lineHeight: i32,
     pub minWidthHeight: i32,
+    #[wasm_bindgen(getter_with_clone)] 
     pub ellipsis: String,
-    pub lineClampProps: LineClampProps,
+    #[wasm_bindgen(getter_with_clone)] 
+    pub line_clamp_props: LineClampProps,
 }
 
+#[wasm_bindgen]
 impl LineClamp {
-    pub fn new() -> LineClamp {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(line_clamp_props: LineClampProps) -> LineClamp {
         LineClamp {
             element: document().create_element("div").unwrap(),
             step: 0.2,
@@ -43,80 +57,25 @@ impl LineClamp {
             lineHeight: 14,
             minWidthHeight: 5,
             ellipsis: String::from("..."),
-            lineClampProps: LineClampProps {
-                texts: Vec::new(),
-                contentWidth: 1,
-                contentHeight: 1,
-            },
-        }
-        
-    }
-
-    pub fn init(&mut self, props: LineClampProps) {
-        self.lineClampProps = props;
-    }
-
-    pub fn calc(&mut self) {
-
-    }
-
-    pub fn transactionToHTML(&self, value: &str) -> String {
-        let format_html = format!("<span style='display:inline-block;'>{}</span>", value.trim());
-        format_html
-    }
-
-    pub fn ellipsisResult(&self) -> InputResultForEllipsis {
-        let mut html_result = Vec::new();
-        let html: String = self.transactionToHTML(&self.ellipsis);
-        html_result.push(html);
-        InputResultForEllipsis {
-            html: html_result,
-            font_size: self.minFontSize
+            line_clamp_props
         }
     }
 
-    pub fn getWordWidth(&self, font_size: i32, incompleteWidth: Vec<i32>) {
-        // if font_size < self.minFontSize {
-        //     IWordInfo {
-        //         text: self.lineClampProps.texts,
-        //         ellipsis: true,
-        //         widths: incompleteWidth,
-        //         font_size: self.minFontSize
-        //     }
-        // }
-    }
-
-    pub fn getWidthOfContent(&self, content: String, fontSize: i32) -> i32 {
-        let div_element = Rc::new(self.element.clone());
-        let div_element_clonet = div_element.clone();
+    
+    pub fn getWidthOfContent(&self, content: String, fontSize: i32) -> i32{
+        let div_element = Rc::new(RefCell::new(self.element.clone()));
+        let div_element_add_attr = div_element.clone();
+        let div_element_set_inner = div_element.clone();
+        let div_element_move = div_element.clone();
         let format_for_style = format!("display:inline-block;font-size:{}px;line-height:{}px", fontSize,self.lineHeight);
-        div_element_clonet.set_attribute("style", &format_for_style);
-        div_element_clonet.set_inner_html(&content);
+        div_element_add_attr.borrow_mut().set_attribute("style", &format_for_style);
+        div_element_set_inner.borrow_mut().set_inner_html(&content);
         let body_tags = document().body().unwrap();
-        body_tags.append_child(&div_element_clonet);
-        let offset_width = <Element as Clone>::clone(&div_element_clonet).dyn_into::<web_sys::HtmlElement>().ok().unwrap().offset_width()
-        body_tags.remove_child(&div_element_clonet);
+        body_tags.append_child(&div_element_move.borrow());
+        let offset_width = <Element as Clone>::clone(&div_element_move.borrow()).dyn_into::<web_sys::HtmlElement>().ok().unwrap().offset_width();
+        body_tags.remove_child(&div_element_move.borrow());
         offset_width
     }    
 
-    pub fn calcSpaceWidth(&self, fontSize: i32) -> i32{
-        let width = self.getWidthOfContent(String::from("5 pace"), fontSize);
-        let word_width = self.getWidthOfContent(String::from("pace"), fontSize);
-        width - word_width
-    }
-    
-    pub fn calc_word_width_can_in_content(result: IWordInfo, contentWidth: i32) {
-        // let groups:Vec<i32> = Vec::new();
-        // let current_group:Vec<usize> = Vec::new();
-        // let current_sum = 0;
-        // let widths = result.widths;
-        // for i in &widths {
-        //     let i = Rc::new(i);
-        //     let i_clone = i.clone();
-        //    if current_sum + **i_clone <= contentWidth.try_into().unwrap() {
-            
-        //    }
-        // }
-    }
 }
 
